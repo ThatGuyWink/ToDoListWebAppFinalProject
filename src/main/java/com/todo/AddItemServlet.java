5611bfc7e42841dcb7b49236daf8afdc55d12a1c
Justin Winkler <152222211+ThatGuyWink@users.noreply.github.com>
2024-11-30 15:02:26 -0500

    Update AddItemServlet.java
    Added logger.

--- src/main/java/com/todo/AddItemServlet.java
+++ src/main/java/com/todo/AddItemServlet.java
@@ -10,19 +10,25 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @WebServlet(name = "addItemServlet", value = "/addItem")
 public class AddItemServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(AddItemServlet.class);
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ToDoListDB");
+
     public AddItemServlet() {
         super();
     }
-    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ToDoListDB");
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String task = req.getParameter("task");
+        logger.info("Received request to add new item with task description: {}", task);
 
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -32,16 +38,19 @@ public class AddItemServlet extends HttpServlet {
             ListItem newItem = new ListItem(task);
             em.persist(newItem);
             tx.commit();
+            logger.info("Successfully added new item: {}", newItem);
         } catch (Exception e) {
             if (tx.isActive()) {
                 tx.rollback();
+                logger.warn("Transaction rolled back due to an error.");
             }
-            e.printStackTrace();
+            logger.error("Error adding new item with task description: {}", task, e);
         } finally {
             em.close();
+            logger.debug("EntityManager closed after adding an item.");
         }
-
 
         resp.sendRedirect("index.jsp");
     }
 }
+
