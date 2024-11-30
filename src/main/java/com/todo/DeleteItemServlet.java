bae1a536fd95fba94272cc2daa205af6cb48fd35
Justin Winkler <152222211+ThatGuyWink@users.noreply.github.com>
2024-11-30 15:02:57 -0500

    Update DeleteItemServlet.java
    Added logger.

--- src/main/java/com/todo/DeleteItemServlet.java
+++ src/main/java/com/todo/DeleteItemServlet.java
@@ -10,17 +10,21 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 @WebServlet(name = "deleteItemServlet", value = "/deleteItem")
 public class DeleteItemServlet extends HttpServlet {
 
+    private static final Logger logger = LoggerFactory.getLogger(DeleteItemServlet.class);
     private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("ToDoListDB");
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String idParam = req.getParameter("id");
+        logger.info("Received request to delete item with ID: {}", idParam);
 
         EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
@@ -32,17 +36,21 @@ public class DeleteItemServlet extends HttpServlet {
             if (item != null) {
                 em.remove(item);
                 tx.commit();
+                logger.info("Successfully deleted item: {}", item);
+            } else {
+                logger.warn("Item not found with ID: {}", id);
             }
         } catch (Exception e) {
             if (tx.isActive()) {
                 tx.rollback();
+                logger.warn("Transaction rolled back due to an error.");
             }
-            e.printStackTrace();
+            logger.error("Error deleting item with ID: {}", idParam, e);
         } finally {
             em.close();
+            logger.debug("EntityManager closed after deleting an item.");
         }
 
-        // Redirect back to the main list page
         resp.sendRedirect("index.jsp");
     }
 }
